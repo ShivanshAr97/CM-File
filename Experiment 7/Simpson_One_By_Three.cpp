b86624aab6481a4ae9ab0f2566a9d1f813540260
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-09 13:36:46 +0530

    commit

---
+++ Experiment 7/Simpson_One_By_Three.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define f(x) 1 / (1 + pow(x, 2))
+using namespace std;
+int main()
+{
+    float lower, upper, stepSize, k, integration = 0.0;
+    int subInterval;
+
+    cout << "Lower limit of integration: ";
+    cin >> lower;
+    cout << "Upper limit of integration: ";
+    cin >> upper;
+    cout << "Number of sub intervals: ";
+    cin >> subInterval;
+
+    stepSize = (upper - lower) / subInterval;
+    integration = f(lower) + f(upper);
+    for (int i = 1; i <= subInterval - 1; i++)
+    {
+        k = lower + i * stepSize;
+        if (i%2==0)
+        {    
+            integration = integration + 2 * (f(k));
+        }
+        else{
+            integration = integration + 4 * (f(k));
+        }
+    }
+
+    integration = integration * stepSize / 3;
+    cout << "Answer is: " << integration;
+
+    return 0;
+}
